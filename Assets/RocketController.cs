18d43e787a3469a2360b40b0fd6d7c2b1ce81c87
Yunus Emre <kahraman03040506@hotmail.com>
2024-03-26 11:22:41 +0300

    RocketAnim

--- Assets/RocketController.cs
+++ Assets/RocketController.cs
@@ -4,6 +4,75 @@ using UnityEngine;
 
 public class RocketController : MonoBehaviour
 {
-    
+    public GameObject[] objectsToCompare; // Karşılaştırılacak objelerin listesi
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        GameObject collidedObject = other.gameObject;
+
+        // Çarpışan nesne üzerindeki aktif olan alt obje sayısını hesapla
+        int collidedActiveChildCount = CountActiveChildObjects(collidedObject.transform);
+
+        // Tanımlanan objeler arasındaki en küçük aktif olan alt obje sayısını bul
+        int minActiveChildCount = int.MaxValue;
+        int minIndex = -1;
+        for (int i = 0; i < objectsToCompare.Length; i++)
+        {
+            if (objectsToCompare[i] != null) // Referans null değilse işlem yap
+            {
+                int activeChildCount = CountActiveChildObjects(objectsToCompare[i].transform);
+                if (activeChildCount < minActiveChildCount)
+                {
+                    minActiveChildCount = activeChildCount;
+                    minIndex = i;
+                }
+            }
+        }
+
+        // Karşılaştırma yap ve sonucu Debug.Log ile yazdır
+        if (collidedActiveChildCount == minActiveChildCount)
+        {
+            Debug.Log("True");
+            other.gameObject.GetComponent<Animation>().Play();
+            // En küçük aktif nesneyi yok et
+            if (minIndex != -1 && objectsToCompare[minIndex] != null) // Referans null değilse işlem yap
+            {
+                Destroy(objectsToCompare[minIndex]);
+                objectsToCompare[minIndex] = null; // Referansı null yap
+            }
+        }
+        else
+        {
+            Debug.Log("False");
+        }
+
+        // Yok etmek yerine devam et
+        //Destroy(other.gameObject);
+    }
+
+    // Verilen transform altındaki aktif olan alt obje sayısını döndüren fonksiyon
+    int CountActiveChildObjects(Transform parent)
+    {
+        int activeChildCount = 0;
+
+        foreach (Transform child in parent)
+        {
+            if (child.gameObject.activeSelf)
+            {
+                activeChildCount++;
+            }
+
+            // Eğer çocuk objenin altında başka çocuk objeler varsa, bu çocuk objenin altındaki aktif olanları da kontrol et
+            if (child.childCount > 0)
+            {
+                activeChildCount += CountActiveChildObjects(child);
+            }
+        }
+
+        return activeChildCount;
+    }
 }
+
+
+
 
---
+++ Assets/RocketManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketManager : MonoBehaviour
+{
+   public GameObject[] rockets; // 4 adet rocket objesini tutacak dizi
+    public int minNumber = 1; // En küçük rastgele sayı
+    public int maxNumber = 7; // En büyük rastgele sayı
+    private List<int> randomNumbers = new List<int>(); // Rastgele sayıları tutacak liste
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateRandomNumbers(); // Rastgele sayıları oluştur
+        ActivateStarsForRockets(); // Yıldızları aktif hale getir
+    }
+
+    // Rastgele sayıları oluşturan fonksiyon
+    void GenerateRandomNumbers()
+    {
+        for (int i = 0; i < 4; i++) // 4 adet rastgele sayı oluştur
+        {
+            int randomNumber;
+            do
+            {
+                randomNumber = Random.Range(minNumber, maxNumber + 1); // Rastgele sayı oluştur
+            } while (randomNumbers.Contains(randomNumber)); // Oluşturulan sayı listede varsa tekrar oluştur
+            randomNumbers.Add(randomNumber); // Oluşturulan sayıyı listeye ekle
+        }
+    }
+
+    // Her bir rocket objesi için belirlenen sayıda yıldızı aktif hale getiren fonksiyon
+    void ActivateStarsForRockets()
+    {
+        for (int i = 0; i < rockets.Length; i++)
+        {
+            GameObject rocket = rockets[i];
+
+            List<Transform> starObjects = new List<Transform>();
+
+            // Rocket altındaki Star taglı yıldızları bul
+            foreach (Transform child in rocket.transform)
+            {
+                if (child.CompareTag("Star") && !child.gameObject.activeSelf)
+                {
+                    starObjects.Add(child);
+                }
+            }
+
+            // Rocket için belirlenen sayıda yıldızı etkinleştir
+            int starsToActivate = randomNumbers[i];
+            for (int j = 0; j < starsToActivate && j < starObjects.Count; j++)
+            {
+                starObjects[j].gameObject.SetActive(true);
+            }
+        }
+    }
+}
